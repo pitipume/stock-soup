3714fc3193917c490bf518feac9de50ac49aa483
Pitipume <pitipume.boonyawan@gmail.com>
2026-05-16 00:59:19 +0700

    fix: allow CORS from server IP

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://77.42.125.240:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
